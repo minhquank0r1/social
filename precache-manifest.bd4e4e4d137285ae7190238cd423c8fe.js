aa7616a5b8d3481ba79296caaedfbf3dd4917333
minhquank0r1 <37474518+minhquank0r1@users.noreply.github.com>
2019-04-04 10:37:55 +0700

    Add files via upload

---
+++ precache-manifest.bd4e4e4d137285ae7190238cd423c8fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2ad9512c7ad4ea794d3a5d6adbd69e",
+    "url": "/static/media/spinner.2f2ad951.gif"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "257e9ac3a850a25c26c8",
+    "url": "/static/js/main.14885461.chunk.js"
+  },
+  {
+    "revision": "01c4736683c0bfa07085",
+    "url": "/static/js/2.5f7a1db4.chunk.js"
+  },
+  {
+    "revision": "257e9ac3a850a25c26c8",
+    "url": "/static/css/main.76341270.chunk.css"
+  },
+  {
+    "revision": "2b56d11d2ce0e82aa14813ef7fdb6770",
+    "url": "/index.html"
+  }
+];
